bb3ad25d342367c4265fbb2cc0153db1b6efd1d4
Digvijay Singh Chouhan <42695092+digvijaychouhan@users.noreply.github.com>
2022-01-26 20:26:32 +0530

    state update

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -1,33 +1,39 @@
-import memedata from "../memesData"
+import {useState}  from "react";
+import memedata from "../memesData";
 
 export default function Meme() {
+    
+    const [memeImage, setMemeImage] = useState("");
+    
     function memeGenerator () {
         const memesArray = memedata.data.memes; 
         const randomNumber = Math.floor(Math.random() * (memesArray.length));
-        const getUrl = memesArray[randomNumber].url;
-        console.log(getUrl);
+        setMemeImage(memesArray[randomNumber].url);
     }
 
     return (
-        <main className="form">
-            <input 
-                type="text" 
-                placeholder="Shut up" 
-                className="form--input"
-            />
-            <input 
-                type="text" 
-                placeholder="and take my money" 
-                className="form--input"
-            />
-            
-            <button 
-                className="form--button"
-                onClick={memeGenerator}
-            >
-                Get a new meme image  🖼
-            </button>
-            
+        <main>
+            <div className="form">
+                <input 
+                    type="text" 
+                    placeholder="Shut up" 
+                    className="form--input"
+                />
+                <input 
+                    type="text" 
+                    placeholder="and take my money" 
+                    className="form--input"
+                />
+                
+                <button 
+                    className="form--button"
+                    onClick={memeGenerator}
+                >
+                    Get a new meme image  🖼
+                </button>
+            </div>           
+
+            <img src={memeImage} alt="" className="meme--image" />
         </main>
     )
 }
